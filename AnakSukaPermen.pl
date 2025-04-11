44424cc07c3b2244dddc0c18030644e26a84030c
Rifama29 <rifamaputra29@gmail.com>
2025-04-11 07:06:21 +0700

    Tugas4

---
+++ AnakSukaPermen.pl
@@ -0,0 +1,12 @@
+anakIbu(andi).
+anakIbu(budi).
+anakIbu(cika).
+anakIbu(dani).
+anakIbu(emil).
+not(anakIbu(fadil)).
+
+sukaPermen(andi).
+sukaPermen(budi).
+sukaPermen(cika).
+not(sukaPermen(dani)).
+not(sukaPermen(emil)).
